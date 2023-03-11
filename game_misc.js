3415ccefb056c117ae3a71ed3aaac8bfc9e34481
Vigitant <xaninsf@gmail.com>
2023-03-11 11:47:43 -0700

    added leading zeroes to numbers less than 5 digits

--- game_misc.js
+++ game_misc.js
@@ -2,11 +2,17 @@ let numDisplay = document.createElement('h2');
 
 function random_number() {
     let numRand = Math.floor(Math.random() * 100000);
+    let paddedNumber = numRand.toString().padStart(5, '0');
+
+    /*let number = 2
+    let result = number.toString().padStart(5, '0')
+    console.log(result); // 00002*/
+
     
     const output = document.getElementById("generated-number");
     removeAllChildNodes(output);
     
-    numDisplay.innerHTML = numRand;
+    numDisplay.innerHTML = paddedNumber;
     output.appendChild(numDisplay);
   }
   
