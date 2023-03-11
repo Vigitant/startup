a619e5227d1da9e3a848ef1e8ed5838dfd673792
Vigitant <xaninsf@gmail.com>
2023-03-11 10:58:32 -0700

    number now generates

--- game_misc.js
+++ game_misc.js
@@ -1,11 +1,15 @@
+let numDisplay = document.createElement('p');
+
 function random_number() {
+    console.log("function called");
     let numRand = Math.floor(Math.random() * 100000);
     
-    const output = document.getElementById("output");
+    const output = document.getElementById("generated-number");
     removeAllChildNodes(output);
     
     numDisplay.innerHTML = numRand;
     output.appendChild(numDisplay);
+    console.log(numRand);
   }
   
   function removeAllChildNodes(parent) {
