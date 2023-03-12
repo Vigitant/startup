378b2a5a7ffaad70876fadb1556c843ccd7267dd
Vigitant <xaninsf@gmail.com>
2023-03-11 21:15:19 -0700

    Started on game feedback messages

--- game_misc.js
+++ game_misc.js
@@ -1,27 +1,36 @@
 let numDisplay = document.createElement('h2');
+let gameMessage = document.createElement('div');
+let messageDisplay = document.createElement('div');
 
 function random_number() {
     let numRand = Math.floor(Math.random() * 100000);
     let paddedNumber = numRand.toString().padStart(5, '0');
-
-    /*let number = 2
-    let result = number.toString().padStart(5, '0')
-    console.log(result); // 00002*/
-
-    
     const output = document.getElementById("generated-number");
     removeAllChildNodes(output);
     
     numDisplay.innerHTML = paddedNumber;
     output.appendChild(numDisplay);
 }
-  
+
+function checkEqualSign () {
+  let userInput = document.getElementById('textArea').value;
+  if (!userInput.includes('=')) {
+    //Oops! Your answer must include at least one =
+    const output = document.getElementById("entryResponse");
+    removeAllChildNodes(output);
+    
+    messageDisplay.innerHTML = "Oops! Your answer must include at least one =";
+    output.appendChild(messageDisplay);
+  }
+}
+
+function resetText() {
+  document.getElementById("textArea").value = "";
+}
+
+
   function removeAllChildNodes(parent) {
     while (parent.firstChild) {
       parent.removeChild(parent.firstChild);
     }
   }
-
-function resetText() {
-    document.getElementById("textArea").value = "";
-}
