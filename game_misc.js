ef84e45b3052a46664328e16410292fcdc98ccb6
Vigitant <xaninsf@gmail.com>
2023-04-06 22:35:02 -0600

    text area stays selected

--- game_misc.js
+++ game_misc.js
@@ -93,12 +93,13 @@ function resetMessage() {
     }
   }
 
-  const insertCharacter = function (character) {              //to fix: text area gets deselected after using button as input
+  const insertCharacter = function (character) {
     let textarea = document.getElementById("textArea");
     let start_position = textarea.selectionStart;
     let end_position = textarea.selectionEnd;
   
     textarea.value = `${textarea.value.substring(0, start_position)}${character}${textarea.value.substring(end_position, textarea.value.length)}`;
+    document.getElementById("textArea").focus();
   };
 
   const backSpace = function() {
