00ab4b4f867d7bb39e6a5596aeef8a22695286dc
Vigitant <xaninsf@gmail.com>
2023-04-10 11:09:11 -0600

    I should commit more often

--- zip_codes.js
+++ zip_codes.js
@@ -1,7 +1,12 @@
 //https://www.zippopotam.us/
 
 async function zipInfo(zipNum) {
-    const response = await fetch('api.zippopotam.us/us/{zipNum}');
+    const response = await fetch(`https://api.zippopotam.us/us/${zipNum}`);
     const data = await response.json();
-    //don't know how to access json data
+    console.log(data.places[0].state);
+
+    const places = data.places[0];
+    console.log(places['place name']);
 }
+
+zipInfo(84602);
