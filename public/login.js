0c156ff476dd59a5e3fc19b6ee14223aa0126b39
Vigitant <xaninsf@gmail.com>
2023-04-17 21:21:49 -0600

    login stuff

--- public/login.js
+++ public/login.js
@@ -9,7 +9,7 @@
     }
 
     if (authenticated) {
-        document.querySelector('#playerName').textContent = userName;
+        document.querySelector('#playerName').textContent = userName;       //work on this
         setDisplay('loginControls', 'none');
         setDisplay('playControls', 'block');
       } else {
@@ -17,3 +17,50 @@
         setDisplay('playControls', 'none');
       }
 })();
+
+async function loginUser() {
+    loginOrCreate(`/api/auth/login`);
+  }
+  
+  async function createUser() {
+    loginOrCreate(`/api/auth/create`);
+  }
+
+  async function loginOrCreate(endpoint) {
+    const userName = document.querySelector('#nameBox')?.value;
+    const password = document.querySelector('#passwordBox')?.value;
+    const response = await fetch(endpoint, {
+      method: 'post',
+      body: JSON.stringify({ email: userName, password: password }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    const body = await response.json();
+  
+    if (response?.status === 200) {
+      localStorage.setItem('userName', userName);
+      window.location.href = 'play.html';
+    } else {
+      const modalEl = document.querySelector('#msgModal');
+      modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+      const msgModal = new bootstrap.Modal(modalEl, {});
+      msgModal.show();
+    }
+  }
+
+  function logout() {
+    fetch(`/api/auth/logout`, {
+      method: 'delete',
+    }).then(() => (window.location.href = '/'));
+  }
+
+  async function getUser(email) {
+    // See if we have a user with the given email.
+    const response = await fetch(`/api/user/${email}`);
+    if (response.status === 200) {
+      return response.json();
+    }
+
+    return null;
+}
